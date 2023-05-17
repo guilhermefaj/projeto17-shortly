419f9f1475aa7457468dea6a1644129abe752fb7
Guilherme <figueiredoguiu@gmail.com>
2023-05-17 09:56:33 -0300

    Chore: Ajuste do código para deploy no Render

--- database/database.connection.js
+++ database/database.connection.js
@@ -1,12 +1,13 @@
-import pg from "pg";
-import dotenv from "dotenv";
+import pg from "pg"
+import dotenv from "dotenv"
+dotenv.config()
 
-dotenv.config();
+const { Pool } = pg
 
-const { Pool } = pg;
-
-const dbConfig = {
-    connectionString: process.env.DATABASE_URL
+const configDatabase = {
+    connectionString: process.env.DATABASE_URL,
 };
 
-export const db = new Pool(dbConfig);
+if (process.env.MODE === "prod") configDatabase.ssl = true;
+
+export const db = new Pool(configDatabase);
