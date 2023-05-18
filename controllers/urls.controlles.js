bbb12fe69bd6ead78e9aa28eb88dbcbe71489baa
Guilherme <figueiredoguiu@gmail.com>
2023-05-18 10:49:06 -0300

    Feat: Get de url por id

--- controllers/urls.controlles.js
+++ controllers/urls.controlles.js
@@ -1,6 +1,5 @@
 import { db } from "../database/database.connection.js";
 import { nanoid } from "nanoid";
-import dayjs from "dayjs";
 
 export async function postUrl(req, res) {
     const { url } = req.body;
@@ -9,7 +8,6 @@ export async function postUrl(req, res) {
     const shortUrl = nanoid();
 
     try {
-        const todayDate = dayjs().format("YYYY-MM-DD");
         const session = res.locals.session;
         const userId = session.userId;
 
@@ -26,5 +24,28 @@ export async function postUrl(req, res) {
         res.status(201).send({ id: linkId, shortUrl: shortUrl });
     } catch (err) {
         res.status(500).send('Erro ao salvar a URL');
+    }
+}
+
+
+export async function getUrl(req, res) {
+    const id = req.params.id;
+
+    try {
+        const result = await db.query(
+            `SELECT "linkId", "shortCode", "url" 
+             FROM links 
+             WHERE "linkId" = $1`,
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).send("URL encurtada não encontrada");
+        }
+
+        const { linkId, shortCode, url } = result.rows[0];
+        res.status(200).send({ id: linkId, shortUrl: shortCode, url });
+    } catch (err) {
+        res.status(500).send("Erro ao obter a URL encurtada");
     }
 }
--- routes/urls.routes.js
+++ routes/urls.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { postUrl } from "../controllers/urls.controlles.js";
+import { getUrl, postUrl } from "../controllers/urls.controlles.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { postUrlSchema } from "../schemas/urls.schemas.js";
 import { authValidation } from "../middlewares/auth.middleware.js";
@@ -7,5 +7,6 @@ import { authValidation } from "../middlewares/auth.middleware.js";
 const urlsRouter = Router();
 
 urlsRouter.post("/urls/shorten", authValidation, validateSchema(postUrlSchema), postUrl);
+urlsRouter.get("/urls/:id", getUrl);
 
 export default urlsRouter;
